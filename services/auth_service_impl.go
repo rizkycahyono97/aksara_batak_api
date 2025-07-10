cfa538cec56cb7e65a9f73f09c155abb54a1e86a
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-07-10 10:11:59 +0700

    add LOGGING in login and register service

--- services/auth_service_impl.go
+++ services/auth_service_impl.go
@@ -11,36 +11,43 @@ import (
 	"github.com/rizkycahyono97/aksara_batak_api/model/web"
 	"github.com/rizkycahyono97/aksara_batak_api/repositories"
 	"golang.org/x/crypto/bcrypt"
+	"log/slog"
 	"time"
 )
 
 type AuthServiceImpl struct {
 	Repo     repositories.AuthRepository
 	Validate *validator.Validate
+	Log      *slog.Logger
 }
 
-func NewAuthService(repo repositories.AuthRepository, validate *validator.Validate) AuthService {
+func NewAuthService(repo repositories.AuthRepository, validate *validator.Validate, log *slog.Logger) AuthService {
 	return &AuthServiceImpl{
 		Repo:     repo,
 		Validate: validate,
+		Log:      log,
 	}
 }
 
 func (s *AuthServiceImpl) Register(ctx context.Context, req web.RegisterUserRequest) (domain.Users, error) {
+	s.Log.InfoContext(ctx, "register process started", "name", req.Password, "email", req.Email)
 	//validation
 	if err := s.Validate.Struct(req); err != nil {
+		s.Log.ErrorContext(ctx, "validation failed for register request", "error", err)
 		return domain.Users{}, err
 	}
 
 	//cek email
 	_, err := s.Repo.FindUserByEmail(ctx, req.Email)
 	if err == nil {
+		s.Log.WarnContext(ctx, "register attempt failed: email has been taken", "email", req.Email)
 		return domain.Users{}, errors.New("user already exists")
 	}
 
 	//hash password
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 	if err != nil {
+		s.Log.WarnContext(ctx, "failed to hash password", "password", req.Password)
 		return domain.Users{}, errors.New("failed to hash password")
 	}
 
@@ -52,30 +59,37 @@ func (s *AuthServiceImpl) Register(ctx context.Context, req web.RegisterUserRequ
 		PasswordHash: string(passwordHash),
 		Role:         "user", // default role
 	}
+	s.Log.DebugContext(ctx, "assign to new object user", "user", userNew)
 
 	//simpan ke database
 	err = s.Repo.CreateUser(ctx, &userNew)
 	if err != nil {
+		s.Log.ErrorContext(ctx, "failed to create new user", "error", err)
 		return domain.Users{}, errors.New("failed to create user")
 	}
 
+	s.Log.DebugContext(ctx, "user registered successfully", "user", userNew)
 	return userNew, nil
 }
 
 func (s *AuthServiceImpl) Login(ctx context.Context, req web.LoginUserRequest) (string, error) {
+	s.Log.InfoContext(ctx, "login process started", "email", req.Email)
 	//validation
 	if err := s.Validate.Struct(req); err != nil {
+		s.Log.ErrorContext(ctx, "validation failed for login request", "error", err)
 		return "", err
 	}
 
 	//cari pengguna
 	user, err := s.Repo.FindUserByEmail(ctx, req.Email)
 	if err != nil {
+		s.Log.WarnContext(ctx, "login attempt failed: user not found", "email", req.Email)
 		return "", errors.New("user not found")
 	}
 
 	//membadingkan password
 	if err := bcrypt.CompareHashAndPassword([]byte(req.Password), []byte(user.PasswordHash)); err != nil {
+		s.Log.WarnContext(ctx, "login attempt failed: password mismatch", "email", req.Email, "userID", user.UUID)
 		return "", errors.New("invalid password")
 	}
 
@@ -104,6 +118,7 @@ func (s *AuthServiceImpl) Login(ctx context.Context, req web.LoginUserRequest) (
 	if err != nil {
 		return "", errors.New("failed to sign token")
 	}
+	s.Log.InfoContext(ctx, "user logged in successfully", "userID", user.UUID)
 
 	return signedToken, nil
 }
