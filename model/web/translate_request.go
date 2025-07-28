a8b8ddb5078ee926e041348222587062140c5ab2
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-07-28 11:00:42 +0700

    add translate V2

--- model/web/translate_request.go
+++ model/web/translate_request.go
@@ -3,20 +3,24 @@ package web
 type TranslateDirection string
 
 const (
-	DirectionBatakToID TranslateDirection = "BATAK_TO_ID"
-	DirectionIDToBatak TranslateDirection = "ID_TO_BATAK"
+	DirectionBatakAksaraToID         TranslateDirection = "batak_aksara_to_id"
+	DirectionBatakAksaraToBatakLatin TranslateDirection = "batak_aksara_to_batak_latin"
+	DirectionBatakLatinToBatakAksara TranslateDirection = "batak_latin_to_batak_aksara"
+	DirectionBatakLatinToID          TranslateDirection = "batak_latin_to_id"
+	DirectionIDToBatakAksara         TranslateDirection = "id_to_batak_aksara"
+	DirectionIDToBatakLatin          TranslateDirection = "id_to_batak_latin"
 )
 
 type TranslateRequest struct {
 	Text string `json:"text" validate:"required,min=1"`
 
 	// Validasi memastikan field ini diisi dan nilainya harus salah satu dari dua pilihan.
-	Direction TranslateDirection `json:"direction" validate:"required,oneof=BATAK_TO_ID ID_TO_BATAK"`
+	Direction TranslateDirection `json:"direction" validate:"required,oneof=batak_aksara_to_id batak_aksara_to_batak_latin batak_latin_to_batak_aksara batak_latin_to_id id_to_batak_aksara id_to_batak_latin"`
 }
 
 func (d TranslateDirection) IsValid() bool {
 	switch d {
-	case DirectionBatakToID, DirectionIDToBatak:
+	case DirectionBatakAksaraToID, DirectionBatakAksaraToBatakLatin, DirectionBatakLatinToBatakAksara, DirectionBatakLatinToID, DirectionIDToBatakAksara, DirectionIDToBatakLatin:
 		return true
 	default:
 		return false
--- utils/helpers/translate_helper.go
+++ utils/helpers/translate_helper.go
@@ -8,11 +8,38 @@ import (
 
 // translate helper untuk prompt direction
 func BuildTranslationPrompt(text string, direction web.TranslateDirection) string {
+	var _ = map[string]map[rune]string{
+		"h":  {'a': "ᯂ", 'i': "ᯂ᯦", 'u': "ᯂᯧ", 'e': "ᯂᯨ", 'o': "ᯂᯩ"},
+		"b":  {'a': "ᯅ", 'i': "ᯅ᯦", 'u': "ᯅᯧ", 'e': "ᯅᯨ", 'o': "ᯅᯩ"},
+		"k":  {'a': "ᯆ", 'i': "ᯆ᯦", 'u': "ᯆᯧ", 'e': "ᯆᯨ", 'o': "ᯆᯩ"},
+		"d":  {'a': "ᯇ", 'i': "ᯇ᯦", 'u': "ᯇᯧ", 'e': "ᯇᯨ", 'o': "ᯇᯩ"},
+		"g":  {'a': "ᯈ", 'i': "ᯈ᯦", 'u': "ᯈᯧ", 'e': "ᯈᯨ", 'o': "ᯈᯩ"},
+		"j":  {'a': "ᯉ", 'i': "ᯉ᯦", 'u': "ᯉᯧ", 'e': "ᯉᯨ", 'o': "ᯉᯩ"},
+		"l":  {'a': "ᯊ", 'i': "ᯊ᯦", 'u': "ᯊᯧ", 'e': "ᯊᯨ", 'o': "ᯊᯩ"},
+		"m":  {'a': "ᯋ", 'i': "ᯋ᯦", 'u': "ᯋᯧ", 'e': "ᯋᯨ", 'o': "ᯋᯩ"},
+		"n":  {'a': "ᯌ", 'i': "ᯌ᯦", 'u': "ᯌᯧ", 'e': "ᯌᯨ", 'o': "ᯌᯩ"},
+		"ng": {'a': "ᯍ", 'i': "ᯍ᯦", 'u': "ᯍᯧ", 'e': "ᯍᯨ", 'o': "ᯍᯩ"},
+		"p":  {'a': "ᯎ", 'i': "ᯎ᯦", 'u': "ᯎᯧ", 'e': "ᯎᯨ", 'o': "ᯎᯩ"},
+		"r":  {'a': "ᯏ", 'i': "ᯏ᯦", 'u': "ᯏᯧ", 'e': "ᯏᯨ", 'o': "ᯏᯩ"},
+		"s":  {'a': "ᯐ", 'i': "ᯐ᯦", 'u': "ᯐᯧ", 'e': "ᯐᯨ", 'o': "ᯐᯩ"},
+		"t":  {'a': "ᯑ", 'i': "ᯑ᯦", 'u': "ᯑᯧ", 'e': "ᯑᯨ", 'o': "ᯑᯩ"},
+		"w":  {'a': "ᯒ", 'i': "ᯒ᯦", 'u': "ᯒᯧ", 'e': "ᯒᯨ", 'o': "ᯒᯩ"},
+		"y":  {'a': "ᯓ", 'i': "ᯓ᯦", 'u': "ᯓᯧ", 'e': "ᯓᯨ", 'o': "ᯓᯩ"},
+	}
+
 	switch direction {
-	case web.DirectionBatakToID:
-		return fmt.Sprintf("Terjemahkan teks Bahasa Batak Toba ini ke Bahasa Indonesia. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: \\\"%s\\\"\"", text)
-	case web.DirectionIDToBatak:
-		return fmt.Sprintf("Terjemahkan teks Bahasa Indonesia ini ke Bahasa Batak Toba. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: \"%s\"", text)
+	case web.DirectionBatakAksaraToID:
+		return fmt.Sprintf(`Terjemahkan teks Aksara Batak Toba ini ke Bahasa Indonesia. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s`, text)
+	case web.DirectionBatakAksaraToBatakLatin:
+		return fmt.Sprintf("Terjemahkan teks Aksara Batak Toba ini ke Bahasa Batak Toba Latin. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s", text)
+	case web.DirectionBatakLatinToID:
+		return fmt.Sprintf("Terjemahkan teks Bahasa Batak Toba ini ke Bahasa Indonesia. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s", text)
+	case web.DirectionBatakLatinToBatakAksara:
+		return fmt.Sprintf("Terjemahkan teks Bahasa Batak Toba Latin ini ke Aksara Batak Toba. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s", text)
+	case web.DirectionIDToBatakLatin:
+		return fmt.Sprintf("Terjemahkan teks Bahasa Indonesia ini ke Bahasa Batak Toba. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s", text)
+	case web.DirectionIDToBatakAksara:
+		return fmt.Sprintf("Terjemahkan teks Bahasa Indonesia ini ke Aksara Batak Toba. Kembalikan HANYA hasil terjemahannya saja, tanpa penjelasan tambahan atau tanda kutip. Teks: %s", text)
 	default:
 		return text
 	}
