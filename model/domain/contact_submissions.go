09bab2aa93e95933de572e6ccc29f96f54973102
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-08-02 10:51:30 +0700

    add model/domain for contact_submissions

---
+++ model/domain/contact_submissions.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type ContactSubmissions struct {
+	ID        uint `json:"id" gorm:"type:bigint;unsigned;not null;autoIncrement:true;unique;autoIncrement:false"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+	Name      string `json:"name" gorm:"type:varchar(255);not null"`
+	Message   string `json:"message" gorm:"type:text;not null"`
+	Status    string `json:"status" gorm:"type:varchar(50);not null;default 'baru'"`
+}
